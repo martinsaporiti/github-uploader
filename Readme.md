04c6862b05cf26e361b373cf38de8e49ab94ac78
Martin Saporiti <martinsaporiti@gmail.com>
2023-05-21 13:49:35 -0300

    first commit

---
+++ Readme.md
@@ -0,0 +1,17 @@
+## How to use:
+
+1. Register Application on Github
+2. Create CLIENT_ID and CLIENT_SECRET env variables:
+
+```bash
+export CLIENT_ID=...
+export CLIENT_SECRET=...
+```
+
+3. Run `go run main.go`
+4. Visit `http://localhost:9000/`
+
+## Refs:
+
+- https://docs.github.com/en/apps/oauth-apps/building-oauth-apps/authorizing-oauth-apps
+- https://clavinjune.dev/en/blogs/serving-embedded-static-file-inside-subdirectory-using-go/
---
+++ main.go
@@ -0,0 +1,156 @@
+package main
+
+import (
+	"context"
+	"embed"
+	"encoding/json"
+	"fmt"
+	"io/fs"
+	"log"
+	"net/http"
+	"os"
+	"path"
+	"text/template"
+
+	"github.com/google/go-github/v52/github"
+	"golang.org/x/oauth2"
+)
+
+// var config = {
+//         method: 'put',
+//         url: 'https://api.github.com/repos/sumit-s03/Test/contents/abc.txt',
+//         headers: {
+//             'Authorization': `Bearer ${token}`,
+//             'Content-Type': 'application/json'
+//         },
+//         data: data
+//     };
+
+var (
+	clientID     string
+	clientSecret string
+)
+
+//go:embed files/static/*
+var staticFiles embed.FS
+
+func main() {
+
+	clientID = os.Getenv("CLIENT_ID")
+	if clientID == "" {
+		log.Fatal("CLIENT_ID env variable is not set")
+	}
+
+	clientSecret = os.Getenv("CLIENT_SECRET")
+	if clientSecret == "" {
+		log.Fatal("CLIENT_SECRET env variable is not set")
+	}
+
+	var staticFS = fs.FS(staticFiles)
+	static, err := fs.Sub(staticFS, "files/static")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./files/static"))))
+	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.FS(static))))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/get_access_token", getAccessToken)
+	log.Printf("Listening on port %d", 9000)
+	log.Fatal(http.ListenAndServe("0.0.0.0:9000", nil))
+
+	// upload()
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	t, _ := template.ParseFiles(path.Join("./files/index.html"))
+	t.Execute(w, "")
+}
+
+func getAccessToken(w http.ResponseWriter, r *http.Request) {
+	code := r.URL.Query().Get("code")
+	postURL := fmt.Sprintf("https://github.com/login/oauth/access_token?client_id=%s&client_secret=%s&code=%s", clientID, clientSecret, code)
+
+	r, err := http.NewRequest("POST", postURL, nil)
+	if err != nil {
+		RespondJSON(w, struct {
+			Error string `json:"error"`
+		}{
+			Error: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+
+	r.Header.Add("Content-Type", "application/json")
+	r.Header.Add("Accept", "application/json")
+	client := &http.Client{}
+	res, err := client.Do(r)
+	if err != nil {
+		RespondJSON(w, struct {
+			Error string `json:"error"`
+		}{
+			Error: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+
+	defer res.Body.Close()
+
+	fmt.Println("response Status:", res.Status)
+	fmt.Println("response Status:", res.Body)
+
+	type AccessToken struct {
+		AccessToken string `json:"access_token"`
+		Scope       string `json:"scope"`
+		TokenType   string `json:"token_type"`
+	}
+
+	at := &AccessToken{}
+	err = json.NewDecoder(res.Body).Decode(at)
+	if err != nil {
+		RespondJSON(w, struct {
+			Error string `json:"error"`
+		}{
+			Error: err.Error(),
+		}, http.StatusInternalServerError)
+		return
+	}
+
+	RespondJSON(w, at, http.StatusOK)
+}
+
+// RespondJSON - translate an interface to json for response
+func RespondJSON(w http.ResponseWriter, resp any, statusCode int) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(statusCode)
+	retJSON, _ := json.Marshal(resp)
+	_, _ = w.Write(retJSON)
+}
+
+func upload() {
+	TOKEN := ""
+	ctx := context.Background()
+	ts := oauth2.StaticTokenSource(
+		&oauth2.Token{AccessToken: TOKEN},
+	)
+
+	tc := oauth2.NewClient(ctx, ts)
+	client := github.NewClient(tc)
+	contentResp, resp, err := client.Repositories.CreateFile(context.Background(), "martinsaporiti", "service_A", "schemas/abc3.txt", &github.RepositoryContentFileOptions{
+		Message: github.String("my commit message"),
+		Content: []byte("Hello World"),
+		Author: &github.CommitAuthor{
+			Name:  github.String("martinsaporiti"),
+			Email: github.String("martinsaporiti@gmail.com"),
+		},
+	})
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer resp.Body.Close()
+
+	fmt.Println("response Status:", resp.Status)
+	fmt.Println("Content Response:", contentResp.GetHTMLURL())
+}
